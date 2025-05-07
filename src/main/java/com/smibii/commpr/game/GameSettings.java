811ebcd4b25c0a2c8b87b0c618a64ddb1808c7bf
SMIBII <54860471+smibii@users.noreply.github.com>
2025-05-07 05:58:23 +0200

    Todo GameSettings.java

---
+++ src/main/java/com/smibii/commpr/game/GameSettings.java
@@ -0,0 +1,11 @@
+// Game settings
+// bool eliminate
+// int lives
+// GameMode mode
+// int maxPlayers
+// int minPlayers
+// int playerCount
+// bool canJoinMidGame
+// int timeLimit (minutes)
+// bool canSpectate
+// bool canFreeCam
