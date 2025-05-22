081fa6931806e0bf3b2579e0306edb3f0324e6b5
SMIBII <54860471+smibii@users.noreply.github.com>
2025-05-22 08:55:06 +0200

    Update TacZLoadout.java

--- src/main/java/com/smibii/commpr/common/tacz/TacZLoadout.java
+++ src/main/java/com/smibii/commpr/common/tacz/TacZLoadout.java
@@ -3,5 +3,9 @@ package com.smibii.commpr.common.tacz;
 import com.tacz.guns.item.ModernKineticGunItem;
 
 public class TacZLoadout {
-    // private final ModernKineticGunItem
+    private final ItemStack primary = PrimaryGun.RIFLE.get(0).getValue();
+    private final ItemStack secondary = SecondaryGun.PISTOL.get(0).getValue();
+    
+    public TacZLoadout() {
+    }
 }
